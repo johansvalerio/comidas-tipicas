e8c1def559c8904023308bc04e9b005a0e427a34
Susanogetsu <johans.valerio@hotmail.com>
2024-03-19 07:35:01 -0600

    Fixed the comidas section

--- src/app/components/Comidas.tsx
+++ src/app/components/Comidas.tsx
@@ -43,19 +43,23 @@ export default function Comidas() {
     const [isActive, setIsActive] = useState("");
 
     const handleClick = (comidasTipicas: any) => {
-        setIsClicked(true);
+        setIsClicked(!isClicked);
         setComida(comidasTipicas.name)
         setImg(comidasTipicas.img)
         setDescription(comidasTipicas.description)
         setPrecio(comidasTipicas.precio)
-        setIsActive(comidasTipicas.name)
+        setIsActive(comidasTipicas.name); // Actualiza el estado isActive al nombre de la comida seleccionada
 
-        if (comida !== comidasTipicas.name) {
+        if (isClicked === true) {
+            if (comida !== comidasTipicas.name) {
+                setIsClicked(true);
+                setIsActive(comidasTipicas.name);
+
+            }else {
+                setIsActive("");
+            }
+        }else {
             setIsClicked(true);
-            setIsActive(comidasTipicas.name)
-        }
-        else {
-            setIsClicked(!isClicked);
         }
     }
 
@@ -70,14 +74,12 @@ export default function Comidas() {
             <div className="text-3xl md:flex md:justify-around mb-10">
                 {comidasTipicas.map((comida: any) => (
                     <h2 key={comida.name}
-                    onClick={() => { handleClick(comida) }}
-                    className={`relative transition duration-1000
+                        onClick={() => { handleClick(comida) }}
+                        className={`relative transition duration-1000
                     text-3xl font-bold text-white text-center mb-5 cursor-pointer
-                    border-b-4 border-transparent hover:border-teal-500
-                    ${isActive === comida.name ? 'border-b-4 border-teal-500' : 'border-b-4 hover:border-gray-200'}
-                    `}>
-                    {comida.name}
-                </h2>
+                    border-b-4 ${isActive === comida.name ? 'border-teal-500' : 'border-transparent hover:border-gray-200'}`}>
+                        {comida.name}
+                    </h2>
                 ))}
             </div>
 
@@ -87,29 +89,28 @@ export default function Comidas() {
                     <div className="flex flex-col items-center justify-center">
                         {
                             isHover === true
-                                && <div onMouseEnter={() => setIsHover(true)} className="z-10 absolute flex flex-col justify-center items-center gap-5 ">
-                                    <h2 className="text-xl sm:text-3xl font-bold">{description}</h2>
-                                    <h2 className="text-xl sm:text-3xl font-bold">Precio: {precio}</h2>
-                                </div>
-                                
-                               
-                        }
-                        <Image src={img} alt={description} width={600} height={300} loading="lazy" 
-                        className={`rounded-lg transition ease-linear ${isHover === true ? "opacity-40" : "opacity-100"}`}
-                        onMouseEnter={() => setIsHover(true)} 
-                        onMouseLeave={() => setIsHover(false)}/>
-                    </div>
-                    : 
-                        <div role="status" className="flex flex-col justify-center items-center rounded shadow animate-pulse dark:border-gray-700">
-                            <h2>Seleccione una comida</h2>
-                            <div className="flex items-center justify-center w-[300px] h-[200px] sm:w-[600px] sm:h-[380px] mb-4 bg-gray-300 rounded dark:bg-gray-700">
-                                <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
-                                    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
-                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
-                                </svg>
+                            && <div onMouseEnter={() => setIsHover(true)} className="z-10 absolute flex flex-col justify-center items-center gap-5 ">
+                                <h2 className="text-xl sm:text-3xl font-bold">{description}</h2>
+                                <h2 className="text-xl sm:text-3xl font-bold">Precio: {precio}</h2>
                             </div>
+
+
+                        }
+                        <Image src={img} alt={description} width={600} height={300} loading="lazy"
+                            className={`rounded-lg transition ease-linear ${isHover === true ? "opacity-40" : "opacity-100"}`}
+                            onMouseEnter={() => setIsHover(true)}
+                            onMouseLeave={() => setIsHover(false)} />
+                    </div>
+                    :
+                    <div role="status" className="flex flex-col justify-center items-center rounded shadow animate-pulse dark:border-gray-700">
+                        <h2>Seleccione una comida</h2>
+                        <div className="flex items-center justify-center w-[600px] h-[380px] mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                            <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                            </svg>
                         </div>
-                   
+                    </div>
             }
         </div>
     )
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
             bg-no-repeat bg-cover ">
 
           <div className="w-full h-full flex flex-col justify-center items-center gap-1">
-            <h1 className="text-lg font-semibold bg-white/80 text-black py-1 px-4">COMIDAS TÍPICAS</h1>
+            {/* <h1 className="text-lg font-semibold bg-white/80 text-black py-1 px-4">COMIDAS TÍPICAS</h1> */}
             <h1 className="text-5xl font-bold bg-black/80 py-4 px-10">EL TAMALITO</h1>
             <div className='flex gap-2 my-5'>
               <button className='bg-stone-50 flex items-center gap-1 text-black font-semibold p-2 rounded-lg hover:bg-stone-200'>Ordenar ahora
