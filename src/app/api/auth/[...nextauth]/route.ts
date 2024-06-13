0df3c0108a0e6acd7b0ebe732f5030a4bca9db00
Susanogetsu <johans.valerio@hotmail.com>
2024-06-13 13:19:05 -0600

    A

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import db from '@/libs/db'
 // import bcrypt from 'bcrypt'
 
- const authOptions = {
+ export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
